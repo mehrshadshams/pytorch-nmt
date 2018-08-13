27a5a797dba83352bfd592df2bb095aa8130c35d
Mehrshad Shams <a-meshams@expedia.com>
2018-08-13 16:34:21 -0400

    Implement Seq2Seq

---
+++ main.py
@@ -0,0 +1,71 @@
+import argparse
+import time
+
+from preprocess import DataProcessor
+from train import Trainer
+
+import torch
+from torchtext import data
+
+
+def main(args):
+    start_time = time.time()
+
+    dp = DataProcessor(args.src_lang, args.trg_lang)
+    train_dataset, valid_dataset, test_dataset, vocabs = dp.load_data('fra.txt', 'english_french.pkl')
+
+    print("Elapsed Time: %1.3f \n" % (time.time() - start_time))
+
+    print("=========== Data Stat ===========")
+    print("Train: ", len(train_dataset))
+    print("Valid: ", len(valid_dataset))
+    print("Test: ", len(test_dataset))
+    print("=================================")
+
+    train_loader = data.BucketIterator(dataset=train_dataset, batch_size=args.batch_size,
+                                       repeat=False, shuffle=True, sort_within_batch=True,
+                                       sort_key=lambda x: len(x.src))
+
+    val_loader = data.BucketIterator(dataset=valid_dataset, batch_size=args.batch_size,
+                                     repeat=False, shuffle=True, sort_within_batch=True,
+                                     sort_key=lambda x: len(x.src))
+
+    trainer = Trainer(train_loader, val_loader, vocabs, args)
+    trainer.train()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # Language setting
+    parser.add_argument('--dataset', type=str, default='europarl')
+    parser.add_argument('--src_lang', type=str, default='fr')
+    parser.add_argument('--trg_lang', type=str, default='en')
+    parser.add_argument('--max_len', type=int, default=50)
+
+    # Model hyper-parameters
+    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--grad_clip', type=float, default=2)
+    parser.add_argument('--num_layer', type=int, default=2)
+    parser.add_argument('--embed_dim', type=int, default=512)
+    parser.add_argument('--hidden_dim', type=int, default=1024)
+
+    # Training setting
+    parser.add_argument('--batch_size', type=int, default=40)
+    parser.add_argument('--num_epoch', type=int, default=100)
+
+    # Path
+    parser.add_argument('--data_path', type=str, default='./data/')
+    parser.add_argument('--train_path', type=str, default='./data/training/europarl-v7.fr-en')
+    parser.add_argument('--val_path', type=str, default='./data/dev/newstest2013')
+
+    # Dir.
+    parser.add_argument('--log', type=str, default='log')
+    parser.add_argument('--sample', type=str, default='sample')
+
+    # Misc.
+    parser.add_argument('--gpu_num', type=int, default=0)
+
+    args = parser.parse_args()
+    print(args)
+    main(args)
---
+++ preprocess.py
@@ -0,0 +1,126 @@
+import re
+import string
+import os
+from unicodedata import normalize
+import numpy as np
+
+import torch
+from torchtext import data, datasets
+
+from utils import *
+
+
+class DataProcessor(object):
+    def __init__(self, src_lng, trg_lng):
+        self.src_field, self.trg_field = self.generate_fields()
+        self.src_lng = src_lng
+        self.trg_lng = trg_lng
+
+    @staticmethod
+    def load_document(filename):
+        with open(filename, mode='rt', encoding='utf-8') as file:
+            text = file.read()
+            lines = text.strip().split('\n')
+            pairs = [line.split('\t') for line in lines]
+
+            return pairs
+
+    @staticmethod
+    def clean_pairs(lines):
+        cleaned = list()
+        # prepare regex for char filtering
+        re_print = re.compile('[^%s]' % re.escape(string.printable))
+        # prepare translation table for removing punctuation
+        table = str.maketrans('', '', string.punctuation)
+        for pair in lines:
+            clean_pair = list()
+            for line in pair:
+                # normalize unicode characters
+                line = normalize('NFD', line).encode('ascii', 'ignore')
+                line = line.decode('UTF-8')
+                # tokenize on white space
+                line = line.split()
+                # convert to lowercase
+                line = [word.lower() for word in line]
+                # remove punctuation from each token
+                line = [word.translate(table) for word in line]
+                # remove non-printable chars form each token
+                line = [re_print.sub('', w) for w in line]
+                # remove tokens with numbers in them
+                line = [word for word in line if word.isalpha()]
+                # store as string
+                clean_pair.append(' '.join(line))
+            cleaned.append(clean_pair)
+        return np.array(cleaned)
+
+    @staticmethod
+    def write_files(data, prefix, src_lang='en', dst_lang='fr'):
+        with open(f'out/{prefix}.{src_lang}', 'wt') as f:
+            f.writelines([line + '\n' for line in data[:, 0]])
+        with open(f'out/{prefix}.{dst_lang}', 'wt') as f:
+            f.writelines([line + '\n' for line in data[:, 1]])
+
+    def load_data(self, input_filename, output_filename):
+        out_file = f'out/{output_filename}'
+
+        if not os.path.exists('out'):
+            os.mkdir('out')
+
+        if not os.path.exists(out_file):
+            print("Preprocessing train dataset...")
+
+            input_filepath = os.path.join('data', input_filename)
+            pairs = DataProcessor.load_document(input_filepath)
+            sentences = DataProcessor.clean_pairs(pairs)
+
+            np.random.shuffle(sentences)
+
+            m = len(sentences)
+
+            train, test = sentences[:int(m * 0.9)], sentences[int(m * 0.9):]
+            valid, test = test[:len(test) // 2], test[len(test) // 2:]
+
+            print("Saving dataset...")
+
+            data = {'train': train, 'valid': valid, 'test': test}
+
+            torch.save(data, out_file)
+
+            for d, p in [(train, 'train'), (valid, 'val'), (test, 'test')]:
+                DataProcessor.write_files(d, p)
+
+        print('Loading datasets...')
+        train_dataset, valid_dataset, test_dataset = datasets.TranslationDataset.splits(path='out',
+                                                                                        exts=('.' + self.src_lng,
+                                                                                              '.' + self.trg_lng),
+                                                                                        fields=(self.src_field, self.trg_field))
+
+        self.src_field.build_vocab(train_dataset, max_size=30000)
+        self.trg_field.build_vocab(train_dataset, max_size=30000)
+
+        src_vocab = self.src_field.vocab.stoi
+        trg_vocab = self.trg_field.vocab.stoi
+
+        # Define index to string vocabs
+        src_inv_vocab = self.src_field.vocab.itos
+        trg_inv_vocab = self.trg_field.vocab.itos
+
+        vocabs = {'src_vocab': src_vocab, 'trg_vocab': trg_vocab,
+                  'src_inv_vocab': src_inv_vocab, 'trg_inv_vocab': trg_inv_vocab}
+
+        return train_dataset, valid_dataset, test_dataset, vocabs
+
+    def generate_fields(self):
+        src_field = data.Field(tokenize=data.get_tokenizer('spacy'),
+                               init_token=SOS_WORD,
+                               eos_token=EOS_WORD,
+                               pad_token=PAD_WORD,
+                               include_lengths=True,
+                               batch_first=True)
+        trg_field = data.Field(tokenize=data.get_tokenizer('spacy'),
+                               init_token=SOS_WORD,
+                               eos_token=EOS_WORD,
+                               pad_token=PAD_WORD,
+                               include_lengths=True,
+                               batch_first=True)
+        return src_field, trg_field
